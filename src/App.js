782c1c74bab6d2df792b2680600b7169be38331c
Anshu-maury <anshumaurya88979@gmail.com>
2024-03-18 21:22:49 +0530

    browse

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
+import Body from './Components/Body';
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-800">Namaste Everyone Lets build Netflix GPT😍😍</div>
+    (<Body />)
   );
 }
 
